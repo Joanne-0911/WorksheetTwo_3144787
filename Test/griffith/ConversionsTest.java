8441961a19beed05e529474cbd6da1ec8a679a3c
Joanne Thomas <you@example.com>
2025-03-07 19:18:30 +0000

    Conversions code

---
+++ Test/griffith/ConversionsTest.java
@@ -0,0 +1,34 @@
+package griffith;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class ConversionsTest {
+
+	@Test
+	void testEuroToDollar() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testDollarToEuro() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testStringToInteger() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testIntegerToString() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testSwitchCase() {
+		fail("Not yet implemented");
+	}
+
+}
